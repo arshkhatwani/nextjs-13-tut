a73da8eedfca4465f90117e582ce61def4afde60
arshkhatwani <arsh.khatwani@gmail.com>
2023-10-22 16:37:48 +0530

    Adds page to add new todos

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,33 @@
+import Link from "next/link";
+
 export default function Page() {
-  return <h1>This is page</h1>;
+  return (
+    <>
+      <header className="flex flex-row justify-between items-center mb-4">
+        <h1 className="text-2xl">New Todo</h1>
+      </header>
+
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+        />
+
+        <div className="flex gap-2 justify-end">
+          <Link
+            href=".."
+            className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100">
+            Cancel
+          </Link>
+
+          <button
+            type="submit"
+            className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100">
+            Create
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
