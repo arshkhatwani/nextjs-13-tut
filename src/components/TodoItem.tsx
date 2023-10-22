b8dbcc37711b036fe3cbf293f9039128ed1a8d0e
arshkhatwani <arsh.khatwani@gmail.com>
2023-10-22 17:29:28 +0530

    Adds function to update todo status

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 type TodoItemProps = {
   id: string;
   title: string;
   complete: boolean;
+  toggleTodo: (id: string, complete: boolean) => void;
 };
 
-export default function TodoItem({ id, title, complete }: TodoItemProps) {
+export default function TodoItem({
+  id,
+  title,
+  complete,
+  toggleTodo,
+}: TodoItemProps) {
   return (
     <li className="flex gap-2 items-center">
-      <input type="checkbox" id={id} className="cursor-pointer peer" />
+      <input
+        type="checkbox"
+        id={id}
+        className="cursor-pointer peer"
+        defaultChecked={complete}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+      />
       <label
         htmlFor={id}
         className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500">
